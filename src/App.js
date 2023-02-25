86e45a9e53d0aedc800bdc58e4af8738ac575c14
Onur Eredo <onureredo@yahoo.com>
2023-02-25 14:37:11 +0100

    react-rnd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { Rnd } from 'react-rnd';
+
 function App() {
+  const style = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: '#f0f0f0',
+    border: 'solid 1px #ddd',
+  };
+
   return (
-    <div className='App'>
+    <Rnd
+      style={style}
+      default={{
+        x: 0,
+        y: 0,
+        width: 600,
+        height: 400,
+      }}
+    >
       <div className='react-dnd'>
-        <h1>MOVE ME AROUND</h1>
+        <p>moveMeAround</p>
       </div>
-    </div>
+    </Rnd>
   );
 }
 
