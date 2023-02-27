18f0fdc8c5e60e5cd2699daede50a7bd934e1f6e
Onur Eredo <onureredo@yahoo.com>
2023-02-27 18:21:36 +0100

    detailed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { useState } from 'react';
 import { Rnd } from 'react-rnd';
 
 function App() {
+  const [state, setState] = useState({
+    x: 5,
+    y: 5,
+    height: '250px',
+    width: '200px',
+  });
+
+  const styleOne = {
+    width: '600px',
+    height: '600px',
+    border: 'solid 1px #ddd',
+    background: 'crimson',
+  };
+
   const style = {
     display: 'flex',
     justifyContent: 'center',
@@ -10,19 +25,36 @@ function App() {
   };
 
   return (
-    <Rnd
-      style={style}
-      default={{
-        x: 0,
-        y: 0,
-        width: 600,
-        height: 400,
-      }}
-    >
-      <div className='react-dnd'>
-        <p>moveMeAround</p>
-      </div>
-    </Rnd>
+    <div className='cool-grid' style={styleOne}>
+      <Rnd
+        style={style}
+        size={{ width: state.width, height: state.height }}
+        position={{ x: state.x, y: state.y }}
+        minHeight={60}
+        minWidth={200}
+        dragGrid={[5, 50]}
+        bounds={'parent'}
+        onDragStop={(e, d) => {
+          setState({ x: d.x, y: d.y });
+        }}
+        onResize={(e, direction, ref, delta, position) => {
+          setState({
+            width: ref.offsetWidth,
+            height: ref.offsetHeight,
+            ...position,
+          });
+        }}
+      >
+        <div className='react-dnd'>
+          <p>moveMeAround</p>
+        </div>
+      </Rnd>
+      <Rnd style={style} bounds={'parent'} dragGrid={[5, 20]}>
+        <div>
+          <p>GRID PART</p>
+        </div>
+      </Rnd>
+    </div>
   );
 }
 
